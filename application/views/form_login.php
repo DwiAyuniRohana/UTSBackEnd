9554e6f694d0c9d0404b0d84cdd5d437b6c37773
Dwi Ayuni Rohana <89717584+DwiAyuniRohana@users.noreply.github.com>
2021-10-21 22:30:00 +0700

    Delete application directory

--- application/views/form_login.php
+++
@@ -1,8 +0,0 @@
-<?php
-echo form_open('index.php/auth/login');
-?>
-<input type="text" name="username">
-<input type="text" name="password">
-<button type="button" name="submit">LOGIN</button>
-</form>
- 
